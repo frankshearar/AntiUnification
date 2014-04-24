c538a1a4f5ae7f8fe13c2b9deabc731c4155ddc0
Frank Shearar <frank.shearar@gmail.com>
2014-04-24 19:31:25 +0100

    Initial import.

---
+++ AntiUnification/AntiUnification.fs
@@ -0,0 +1,79 @@
+﻿module AntiUnification
+
+type Symbol = string
+
+type Term<'a> =
+    | Function of Symbol * Term<'a> list
+    | Val of 'a
+    | Var of string
+
+type TermSequence<'a> = Term<'a> list
+
+// ('a -> bool) -> ['a] -> bool
+let andMap f xs =
+    List.fold (fun a b -> a && b) true (List.map f xs)
+
+// Given a term sequence (of Functions), return a list of Functions whose contained lists contain the heads of the original Functions
+let heads xs: Term<'a> list =
+    List.map (function
+        | Function (name, [t]) -> t
+        | Function (name, t::ts) ->
+            Function (name, [t])
+        | unrecognised -> failwith (sprintf "heads can only process a Function, not %A" unrecognised)) xs
+   
+// Given a term sequence (of Functions), return a list of Functions whose lists are the tails of the original Functions
+let tails xs =
+    List.map (function
+        | Function (name, [t]) -> Function (name, [])
+        | Function (name, t::ts) ->
+            Function (name, ts)
+        | unrecognised -> failwith (sprintf "tails can only process a Function, not %A" unrecognised)) xs
+
+let has_map key substitution =
+    List.exists (fun e -> fst e = key) substitution
+
+let find_map term_sequence substitution =
+    List.find (fun e -> fst e = term_sequence) substitution
+
+// Does the substitution contain a mapping to a logical variable?
+let has_var term_sequence theta =
+    List.exists (function
+        | (term_sequence, Var _) -> true
+        | _ -> false) theta
+
+let var base_name n =
+    Var (sprintf "%s%d" base_name n)
+
+// At the moment, we use logical variables of the form Var "#z0", Var "#z1", Var "#z2", ....
+// Theta maps term sequences to Vars. That is, it accumulates the parts of the examples that differ.
+// n contains the number of logical variables found so far: it lets us create fresh logical variables.
+let rec antiUnifyTheta examples (theta: (TermSequence<'a> * Term<'a>) list) n =
+    printfn "antiUnifyTheta %A" examples
+    match examples with
+    | [] -> failwith "Cannot invoke antiUnifyTheta with []" // Required for completeness, but antiUnify guarantees that examples is not empty
+    | t::ts when andMap (fun t' -> t' = t) examples -> (t, theta, n) // rule 7: all examples the same? return the first
+    | ((Function (f, args)) as t)::ts when (andMap (function
+                                                 | Function (this_name, these_args) -> f = this_name && args.Length = these_args.Length // Function name & arity count!
+                                                 | _ -> false) examples) -> // rule 8: recurse into the Function arguments.
+        printfn "recursion1 into %A" (heads (t::ts))
+        let (s, theta', n) = antiUnifyTheta (heads (t::ts)) theta n
+        printfn "recursion2 into %A" (tails (t::ts))
+        let (tails_au, theta'', n) = antiUnifyTheta (tails (t::ts)) theta' n
+        let au_of_args = match tails_au with
+                         | (Function (g, ss)) -> Function (f, s::ss)
+                         | Var name -> Function (f, (s::[Var name]))
+                         | unrecognised -> failwith (sprintf "recursion2: %A" unrecognised)
+        (au_of_args, theta'', n)
+    | ts when has_map ts theta -> // rule 9: return the previously mapped logical variable
+        let (term_sequence, mapped_var) = find_map ts theta
+        (mapped_var, theta, n)
+    | ts -> // rule 10: introduce a fresh logical variable
+        let z = var "#z" n // This hardcoded base name lets us avoid passing in a name generator. A post-process step could always replace these. But what if someone wants to use "#z" prefixed names....!
+        (z, ((ts, z) :: theta), n + 1)
+    | _ -> failwith "There is no rule 11" // Necessary because all the previous matches have when clauses
+
+let antiUnify = function
+    | [] -> None
+    | examples  ->
+        let (generalised_example, _, _) = antiUnifyTheta examples [] 0 in
+            Some generalised_example
---
+++ AntiUnificationTests/AntiUnificationTests.fs
@@ -0,0 +1,39 @@
+﻿module TermTests
+
+open AntiUnification
+open NUnit.Framework
+
+let check (a: 'a option) (b: 'a option) =
+    Assert.AreEqual(a, b, sprintf "%A != %A" a b)
+
+[<TestFixture>]
+type anti_unification() =
+    [<Test>]
+    member this.must_have_at_least_one_example() =
+        check None (antiUnify [])
+    [<Test>]
+    member this.anti_unification_of_single_example_is_that_example() =
+        let x = Function ("f", [Val 1])
+        check (Some x) (antiUnify [x])
+    [<Test>]
+    // I'd like to use a Theory here but the type of the Datapoints needs to be Term<'a> list list _for a specific 'a_.
+    // Can't use Term<System.Object> because an int isn't one of those!
+    member this.anti_unification_of_simple_ununifiable_examples_is_logical_var() =
+        check (Some (Var "#z0")) (antiUnify [Val 1; Val 2])
+        check (Some (Var "#z0")) (antiUnify [Val 1; Val 2; Val 3])
+        check (Some (Var "#z0")) (antiUnify [Var "x"; Val "y"])
+        check (Some (Var "#z0")) (antiUnify [Var "#z0"; Val "y"])
+        check (Some (Var "#z0")) (antiUnify [Var "y"; Val "#z0"])
+        check (Some (Var "#z0")) (antiUnify [Function ("foo", []); Function ("bar", [])])
+    [<Test>]
+    member this.anti_unification_of_ununifiable_examples_is_logical_var() =
+        check (Some(Var "#z0")) (antiUnify [Function ("f", [Val 5]); Function ("g", [Val 6])])
+    [<Test>]
+    member this.function_arity_counts() =
+        check (Some(Var "#z0")) (antiUnify [Function ("f", [Val 5]); Function ("f", [Val 5; Val 5])])
+    [<Test>]
+    member this.function_name_counts() =
+        check (Some(Var "#z0")) (antiUnify [Function ("f", [Val 5]); Function ("g", [Val 5])])
+    [<Test>]
+    member this.parts_of_a_structure_may_become_logical_vars() =
+        check (Some (Function ("f", [Var "#z0"]))) (antiUnify [Function ("f", [Val 5]); Function ("f", [Val 6])])
