23c3a9f833982952481c730fa24ddb60919c57df
Frank Shearar <frank.shearar@skype.net>
2014-05-28 19:38:12 +0100

    Now you can't even ask the question 'what is the antiunification of no examples.

--- AntiUnification/AntiUnification.fs
+++ AntiUnification/AntiUnification.fs
@@ -8,23 +8,41 @@ type Term<'a> =
     | Var of string
     | Const of string
 
-type TermSequence<'a> = Term<'a> list
+type TermSequence<'a> =
+    | One of 'a
+    | Many of 'a * TermSequence<'a>
 
-// and_map returns true iff a predicate is true for every element in a list
-// ('a -> bool) -> 'a list -> bool
+let rec fold f seed = function
+    | One x -> f seed x
+    | Many (x, xs) -> fold f x xs
+
+let rec fromList = function
+    | [] -> failwith "Cannot convert [] to a NonEmptyList"
+    | t::[] -> One t
+    | t::ts -> Many (t, fromList ts)
+
+let head = function
+    | One x -> x
+    | Many (x, xs) -> x
+
+let rec map f = function
+    | One x -> One (f x)
+    | Many (x, xs) -> Many (f x, map f xs)
+
+// and_map returns true iff a predicate is true for every element in a non-empty list
 let andMap f xs =
-    List.fold (&&) true (List.map f xs)
+    fold (&&) true (map f xs)
 
 // heads returns a list of Functions whose contained lists contain the heads of the original Functions when given a term sequence (of Functions)
-let heads xs: Term<'a> list =
-    List.map (function
+let heads xs =
+    map (function
         | Function (name, [t]) -> t
         | Function (name, t::ts) -> Function (name, [t])
         | unrecognised -> failwith (sprintf "heads can only process a Function, not %A" unrecognised)) xs
    
 // tails returns a list of Functions whose lists are the tails of the original Functions when given a term sequence (of Functions)
 let tails xs =
-    List.map (function
+    map (function
         | Function (name, [t]) -> Function (name, [])
         | Function (name, t::ts) ->
             Function (name, ts)
@@ -41,39 +59,56 @@ let find_map term_sequence substitution =
 // has_var returns true iff the substitution contain a mapping to a logical variable
 let has_var term_sequence theta =
     List.exists (function
-        | (term_sequence, Var _) -> true
-        | _ -> false) theta
+        | _, Var _ -> true
+        | _, Const _
+        | _, Function (_, _)
+        | _, Val _ -> false) theta
 
 // base_name turns a prefix and a number into a Var
 let var base_name n =
     Var (sprintf "%s%d" base_name n)
+
+// allSame returns true if all elements in the sequence are equal to the first one.
+let allSame l =
+    let first = head l
+    andMap (fun each -> each = first) l
+
+// unifiable returns true if and only if all elements in the sequence are Functions with
+// the same name and the same arity.
+let possiblyUnifiable examples =
+    match examples with
+    | Many (Function (f, args), _) as examples ->
+        (andMap (function
+                 | Function (this_name, these_args) -> f = this_name && args.Length = these_args.Length
+                 | _ -> false) examples)
+    | Many (Const _, _)
+    | Many (Val _, _)
+    | Many (Var _, _)
+    | One _ -> false
 
 // antiUnifyTheta is a helper for antiUnify, returning the anti-unification of a term sequence with
 // respect to a substitution map (mapping term sequences to logical variables)
 // n contains the number of logical variables found so far: it lets us create fresh logical variables.
 // TODO: At the moment, we use logical variables of the form Var "#z0", Var "#z1", Var "#z2", ... This
 // will cause problems is a user happens to also use names in this namespace.
-let rec antiUnifyTheta examples (theta: (TermSequence<'a> * Term<'a>) list) n =
-    match examples with
-    | [] -> failwith "Cannot invoke antiUnifyTheta with []" // Needed for completeness' sake: antiUnify already protects us from [].
-    | t::ts when andMap (fun t' -> t' = t) examples -> (t, theta, n) // rule 7: all examples the same? return the first
-    | ((Function (f, args)) as t)::ts when (andMap (function
-                                                 | Function (this_name, these_args) -> f = this_name && args.Length = these_args.Length // Function name & arity count!
-                                                 | _ -> false) examples) -> // rule 8: recurse into the Function arguments.
-        let (s, theta', n) = antiUnifyTheta (heads (t::ts)) theta n
-        let (tails_au, theta'', n) = antiUnifyTheta (tails (t::ts)) theta' n
-        let au_of_args = match tails_au with
-                         | (Function (g, ss)) -> Function (f, s::ss)
+let rec antiUnifyTheta (theta: (TermSequence<Term<'a>> * Term<'a>) list) n = function
+    | One x -> (x, theta, n)
+    | Many (t, _) as examples when allSame examples -> (t, theta, n) // rule 7: all examples the same? return the first
+    | Many ((Function (f, args)), _) as examples when possiblyUnifiable examples -> // rule 8: recurse into the Function arguments.
+        let s, theta', n = antiUnifyTheta theta n (heads examples)
+        let tails_au, theta'', n = antiUnifyTheta theta' n (tails examples)
+        let au_of_args = match tails_au with // How can we be sure we only ever have Functions and Vars here?
+                         | Function (g, ss) -> Function (f, s::ss)
                          | Var name -> Function (f, (s::[Var name]))
                          | unrecognised -> failwith (sprintf "unrecognised term %A while walking function argument tails" unrecognised)
-        (au_of_args, theta'', n)
-    | ts when has_map ts theta -> // rule 9: return the previously mapped logical variable
-        let (term_sequence, mapped_var) = find_map ts theta
-        (mapped_var, theta, n)
-    | ts -> // rule 10: introduce a fresh logical variable
+        au_of_args, theta'', n
+    | Many _ as ts when has_map ts theta -> // rule 9: return the previously mapped logical variable
+        let term_sequence, mapped_var = find_map ts theta
+        mapped_var, theta, n
+    | Many _ as ts -> // rule 10: introduce a fresh logical variable
         let z = var "#z" n // This hardcoded base name lets us avoid passing in a name generator. A post-process step could always replace these. But what if someone wants to use "#z" prefixed names....!
-        (z, ((ts, z) :: theta), n + 1)
-    | unrecognised -> failwith (sprintf "There is no rule 11: %A" unrecognised) // Necessary because all the previous matches have when clauses
+        z, ((ts, z) :: theta), n + 1
+    | unrecognised -> failwith (sprintf "There is no rule 11: %A" unrecognised) // Necessary because all the previous matches have when clauses, so we can't rely on F#'s exhaustiveness checking
 
 // preprocess turns all Vars into ground terms by pretending they're constant terms
 let rec preprocess = function
@@ -88,9 +123,7 @@ let rec postprocess = function
     | x -> x
 
 // antiUnify returns the least general generalisation of a list of example terms (a term sequence).
-let antiUnify = function
-    | [] -> None
-    | examples ->
-        let processed_examples = List.map preprocess examples
-        let (generalised_example, _, _) = antiUnifyTheta processed_examples [] 0
-        Some (postprocess generalised_example)
+let antiUnify examples =
+    let processed_examples = map preprocess examples
+    let generalised_example, _, _ = antiUnifyTheta [] 0 processed_examples
+    Some (postprocess generalised_example)
--- AntiUnificationTests/AntiUnificationTests.fs
+++ AntiUnificationTests/AntiUnificationTests.fs
@@ -9,39 +9,36 @@ let check (a: 'a option) (b: 'a option) =
 [<TestFixture>]
 type ``Anti-unification``() =
     [<Test>]
-    member this.``must have at least one example``() =
-        check None (antiUnify [])
-    [<Test>]
     member this.``anti-unification of single example is that example``() =
         let x = Function ("f", [Val 1])
-        check (Some x) (antiUnify [x])
+        check (Some x) (antiUnify (One x))
     [<Test>]
     // I'd like to use a Theory here but the type of the Datapoints needs to be Term<'a> list list  for a specific 'a_.
     // Can't use Term<System.Object> because an int isn't one of those!
     member this.``anti unification of simple ununifiable examples is logical var``() =
-        check (Some (Var "#z0")) (antiUnify [Val 1; Val 2])
-        check (Some (Var "#z0")) (antiUnify [Val 1; Val 2; Val 3])
-        check (Some (Var "#z0")) (antiUnify [Var "x"; Val "y"])
-        check (Some (Var "#z0")) (antiUnify [Var "#z0"; Val "y"])
-        check (Some (Var "#z0")) (antiUnify [Var "y"; Val "#z0"])
-        check (Some (Var "#z0")) (antiUnify [Function ("foo", []); Function ("bar", [])])
+        check (Some (Var "#z0")) (antiUnify (fromList [Val 1; Val 2]))
+        check (Some (Var "#z0")) (antiUnify (fromList [Val 1; Val 2; Val 3]))
+        check (Some (Var "#z0")) (antiUnify (fromList [Var "x"; Val "y"]))
+        check (Some (Var "#z0")) (antiUnify (fromList [Var "#z0"; Val "y"]))
+        check (Some (Var "#z0")) (antiUnify (fromList [Var "y"; Val "#z0"]))
+        check (Some (Var "#z0")) (antiUnify (fromList [Function ("foo", []); Function ("bar", [])]))
     [<Test>]
     member this.``anti-unification of ununifiable examples is logical var``() =
-        check (Some(Var "#z0")) (antiUnify [Function ("f", [Val 5]); Function ("g", [Val 6])])
+        check (Some(Var "#z0")) (antiUnify (fromList [Function ("f", [Val 5]); Function ("g", [Val 6])]))
     [<Test>]
     member this.can_generalise_structures_with_logical_vars() =
-        check (Some (Var "#z0")) (antiUnify [Var "#z0"; Var "#z0"])
-        check (Some (Function ("f", [Var "#z0"]))) (antiUnify [Function ("f", [Val 5]); Function ("f", [Var "var"])])
-        check (Some (Function ("f", [Var "#z0"]))) (antiUnify [Function ("f", [Var "var"]); Function ("f", [Val 5])])
+        check (Some (Var "#z0")) (antiUnify (fromList [Var "#z0"; Var "#z0"]))
+        check (Some (Function ("f", [Var "#z0"]))) (antiUnify (fromList [Function ("f", [Val 5]); Function ("f", [Var "var"])]))
+        check (Some (Function ("f", [Var "#z0"]))) (antiUnify (fromList [Function ("f", [Var "var"]); Function ("f", [Val 5])]))
     [<Test>]
     member this.``function arity counts``() =
-        check (Some(Var "#z0")) (antiUnify [Function ("f", [Val 5]); Function ("f", [Val 5; Val 5])])
+        check (Some(Var "#z0")) (antiUnify (fromList [Function ("f", [Val 5]); Function ("f", [Val 5; Val 5])]))
     [<Test>]
     member this.``function name counts``() =
-        check (Some(Var "#z0")) (antiUnify [Function ("f", [Val 5]); Function ("g", [Val 5])])
+        check (Some(Var "#z0")) (antiUnify (fromList [Function ("f", [Val 5]); Function ("g", [Val 5])]))
     [<Test>]
     member this.``parts of a structure may become logical vars``() =
-        check (Some (Function ("f", [Var "#z0"]))) (antiUnify [Function ("f", [Val 5]); Function ("f", [Val 6])])
+        check (Some (Function ("f", [Var "#z0"]))) (antiUnify (fromList [Function ("f", [Val 5]); Function ("f", [Val 6])]))
 
 type BinaryTree<'a> =
     | Node of BinaryTree<'a> * BinaryTree<'a>
@@ -59,4 +56,4 @@ type ``Anti-unification of arbitrary types``() =
         let tree2 = Node (Leaf 1, Leaf 3)
         let term1 = toTerm tree1
         let term2 = toTerm tree2
-        check (Some (Function ("Node",[Function ("Node",[Val 1]); Var "#z0"]))) (antiUnify [term1; term2])
+        check (Some (Function ("Node",[Function ("Node",[Val 1]); Var "#z0"]))) (antiUnify (fromList [term1; term2]))
