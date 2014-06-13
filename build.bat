e4655c6f3e7d58527301a9aaa32b7cfae6fab7a5
Frank Shearar <frank.shearar@skype.net>
2014-06-13 21:17:26 +0100

    Actually, the small sin of keeping an exe in the repo more than pays for itself by not downloading and running a random executable off the internet...

--- build.bat
+++ build.bat
@@ -1,10 +1,6 @@
 @echo off
 cls
 
-if exist ".nuget\NuGet.exe" (
-   ".nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
-) else (
-  nuget "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
-)
+".nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
 
 "packages\FAKE\tools\Fake.exe" build.fsx
