c565eca5a4a864339475598e18eeae4bda80d526
Frank Shearar <frank.shearar@skype.net>
2014-06-14 14:59:00 +0100

    Not a perms problem. Let's try be less specific about framework version.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
-mono --runtime=v4.0 .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
-mono --runtime=v4.0 packages/FAKE/tools/FAKE.exe build.fsx $@
+mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+mono packages/FAKE/tools/FAKE.exe build.fsx $@
